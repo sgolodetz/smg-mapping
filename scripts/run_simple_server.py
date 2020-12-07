a9d93fb29e8912ca4ecf5e3669fa5ef1bbc8c11e
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-12-07 18:18:19 +0000

    Minor tidying.

--- scripts/run_simple_server.py
+++ scripts/run_simple_server.py
@@ -5,24 +5,31 @@ from smg.mapping.remote import MappingServer, RGBDFrameMessageUtil, RGBDFrameRec
 
 def main() -> None:
     with MappingServer(frame_decompressor=RGBDFrameMessageUtil.decompress_frame_message) as server:
-        server.start()
-
         client_id: int = 0
         receiver: RGBDFrameReceiver = RGBDFrameReceiver()
         seen_frame: bool = False
 
+        # Start the server.
+        server.start()
+
+        # While the client hasn't terminated:
         while server.has_more_frames(client_id):
+            # If an RGB-D frame is currently available:
             if server.has_frames_now(client_id):
+                # Get it and show it. Scale the depth image to make it visible.
                 server.get_frame(client_id, receiver)
                 cv2.imshow("Received RGB Image", receiver.get_rgb_image())
-                cv2.imshow("Received Depth Image", receiver.get_depth_image() * 10)  # Multiply by 10 to make it visible
+                cv2.imshow("Received Depth Image", receiver.get_depth_image() * 10)
                 seen_frame = True
 
+            # If we've ever seen a frame:
             if seen_frame:
+                # Run the OpenCV event loop, and break if the user presses 'q'.
                 c: int = cv2.waitKey(1)
                 if c == ord('q'):
                     break
 
+        # Make sure all of the OpenCV windows are destroyed.
         cv2.destroyAllWindows()
 
 
