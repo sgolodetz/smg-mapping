be4e93a8f25523bef9012a038d9a6ba11c1beffe
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-18 13:10:01 +0000

    Import the MVDepth -> Open3D server from the mvdepthserver-o3d branch.

---
+++ scripts/run_mvdepth_octomap_server.py
@@ -0,0 +1,142 @@
+import numpy as np
+import os
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+
+from argparse import ArgumentParser
+from OpenGL.GL import *
+from timeit import default_timer as timer
+from typing import Optional, Tuple
+
+from smg.mapping.remote import MappingServer, RGBDFrameMessageUtil, RGBDFrameReceiver
+from smg.mvdepthnet import MonocularDepthEstimator
+from smg.opengl import OpenGLUtil
+from smg.pyoctomap import *
+from smg.rigging.cameras import SimpleCamera
+from smg.rigging.controllers import KeyboardCameraController
+from smg.utility import GeometryUtil
+
+
+def main() -> None:
+    np.set_printoptions(suppress=True)
+
+    # Parse any command-line arguments.
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--camera_mode", "-m", type=str, choices=("follow", "free"), default="free",
+        help="the camera mode"
+    )
+    args: dict = vars(parser.parse_args())
+
+    # Initialise PyGame and create the window.
+    pygame.init()
+    window_size: Tuple[int, int] = (640, 480)
+    pygame.display.set_mode(window_size, pygame.DOUBLEBUF | pygame.OPENGL)
+    pygame.display.set_caption("MVDepth -> Octomap Server")
+
+    # Enable the z-buffer.
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LESS)
+
+    # Set up the octree drawer.
+    drawer: OcTreeDrawer = OcTreeDrawer()
+    drawer.set_color_mode(CM_COLOR_HEIGHT)
+
+    # Create the octree.
+    voxel_size: float = 0.05
+    tree: OcTree = OcTree(voxel_size)
+
+    # Construct the camera controller.
+    camera_controller: KeyboardCameraController = KeyboardCameraController(
+        SimpleCamera([0, 0, 0], [0, 0, 1], [0, -1, 0]), canonical_angular_speed=0.05, canonical_linear_speed=0.1
+    )
+
+    with MappingServer(frame_decompressor=RGBDFrameMessageUtil.decompress_frame_message) as server:
+        client_id: int = 0
+        depth_estimator: Optional[MonocularDepthEstimator] = None
+        intrinsics: Optional[Tuple[float, float, float, float]] = None
+        receiver: RGBDFrameReceiver = RGBDFrameReceiver()
+        tracker_w_t_c: Optional[np.ndarray] = None
+
+        # Start the server.
+        server.start()
+
+        while True:
+            # Process any PyGame events.
+            for event in pygame.event.get():
+                # If the user wants to quit:
+                if event.type == pygame.QUIT:
+                    # If the reconstruction process has actually started:
+                    if tracker_w_t_c is not None:
+                        # Save the current octree to disk.
+                        print("Saving octree to remote_fusion_mvdepth.bt")
+                        tree.write_binary("remote_fusion_mvdepth.bt")
+
+                    # Shut down pygame, and forcibly exit the program.
+                    pygame.quit()
+                    # noinspection PyProtectedMember
+                    os._exit(0)
+
+            # If the server has a frame from the client that has not yet been processed:
+            if server.has_frames_now(client_id):
+                # Get the camera intrinsics from the server.
+                intrinsics = server.get_intrinsics(client_id)[0]
+
+                # Get the frame from the server.
+                server.get_frame(client_id, receiver)
+                colour_image: np.ndarray = receiver.get_rgb_image()
+                tracker_w_t_c = receiver.get_pose()
+
+                # If the depth estimator hasn't been constructed yet, construct it now.
+                if depth_estimator is None:
+                    depth_estimator = MonocularDepthEstimator(
+                        "C:/Users/Stuart Golodetz/Downloads/MVDepthNet/opensource_model.pth.tar",
+                        GeometryUtil.intrinsics_to_matrix(intrinsics)
+                    )
+
+                # TODO: Comment here.
+                estimated_depth_image: Optional[np.ndarray] = depth_estimator.estimate_depth(
+                    colour_image, tracker_w_t_c
+                )
+
+                # TODO: Comment here.
+                if estimated_depth_image is not None:
+                    # TODO
+                    estimated_depth_image = np.where(estimated_depth_image <= 3.0, estimated_depth_image, 0.0)
+
+                    # Use the depth image and pose to make an Octomap point cloud.
+                    pcd: Pointcloud = OctomapUtil.make_point_cloud(estimated_depth_image, tracker_w_t_c, intrinsics)
+
+                    # Fuse the point cloud into the octree.
+                    start = timer()
+                    origin: Vector3 = Vector3(0.0, 0.0, 0.0)
+                    tree.insert_point_cloud(pcd, origin, discretize=True)
+                    end = timer()
+                    print(f"  - Time: {end - start}s")
+
+            # Allow the user to control the camera.
+            camera_controller.update(pygame.key.get_pressed(), timer() * 1000)
+
+            # Clear the colour and depth buffers.
+            glClearColor(1.0, 1.0, 1.0, 1.0)
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+            # Once the pose is available:
+            if tracker_w_t_c is not None:
+                # Set the projection matrix.
+                glMatrixMode(GL_PROJECTION)
+                OpenGLUtil.set_projection_matrix(intrinsics, *window_size)
+
+                # Draw the octree.
+                viewing_pose: np.ndarray = \
+                    np.linalg.inv(tracker_w_t_c) if args["camera_mode"] == "follow" \
+                    else camera_controller.get_pose()
+                OctomapUtil.draw_octree(tree, viewing_pose, drawer)
+
+            # Swap the front and back buffers.
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
--- scripts/run_mvdepth_open3d_server.py
+++ scripts/run_mvdepth_open3d_server.py
@@ -1,4 +1,5 @@
 import numpy as np
+import open3d as o3d
 import os
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
@@ -11,11 +12,11 @@ from typing import Optional, Tuple
 
 from smg.mapping.remote import MappingServer, RGBDFrameMessageUtil, RGBDFrameReceiver
 from smg.mvdepthnet import MonocularDepthEstimator
+from smg.open3d import ReconstructionUtil, VisualisationUtil
 from smg.opengl import OpenGLUtil
-from smg.pyoctomap import *
 from smg.rigging.cameras import SimpleCamera
 from smg.rigging.controllers import KeyboardCameraController
-from smg.utility import GeometryUtil
+from smg.utility import GeometryUtil, ImageUtil
 
 
 def main() -> None:
@@ -33,19 +34,18 @@ def main() -> None:
     pygame.init()
     window_size: Tuple[int, int] = (640, 480)
     pygame.display.set_mode(window_size, pygame.DOUBLEBUF | pygame.OPENGL)
-    pygame.display.set_caption("Octomap MVDepth Server")
+    pygame.display.set_caption("MVDepth -> Open3D Server")
 
     # Enable the z-buffer.
     glEnable(GL_DEPTH_TEST)
     glDepthFunc(GL_LESS)
 
-    # Set up the octree drawer.
-    drawer: OcTreeDrawer = OcTreeDrawer()
-    drawer.set_color_mode(CM_COLOR_HEIGHT)
-
-    # Create the octree.
-    voxel_size: float = 0.05
-    tree: OcTree = OcTree(voxel_size)
+    # Create the TSDF.
+    tsdf: o3d.pipelines.integration.ScalableTSDFVolume = o3d.pipelines.integration.ScalableTSDFVolume(
+        voxel_length=0.01,
+        sdf_trunc=0.04,
+        color_type=o3d.pipelines.integration.TSDFVolumeColorType.RGB8
+    )
 
     # Construct the camera controller.
     camera_controller: KeyboardCameraController = KeyboardCameraController(
@@ -69,9 +69,9 @@ def main() -> None:
                 if event.type == pygame.QUIT:
                     # If the reconstruction process has actually started:
                     if tracker_w_t_c is not None:
-                        # Save the current octree to disk.
-                        print("Saving octree to remote_fusion_mvdepth.bt")
-                        tree.write_binary("remote_fusion_mvdepth.bt")
+                        # Visualise the TSDF.
+                        mesh: o3d.geometry.TriangleMesh = ReconstructionUtil.make_mesh(tsdf, print_progress=True)
+                        VisualisationUtil.visualise_geometry(mesh)
 
                     # Shut down pygame, and forcibly exit the program.
                     pygame.quit()
@@ -92,7 +92,8 @@ def main() -> None:
                 if depth_estimator is None:
                     depth_estimator = MonocularDepthEstimator(
                         "C:/Users/Stuart Golodetz/Downloads/MVDepthNet/opensource_model.pth.tar",
-                        GeometryUtil.intrinsics_to_matrix(intrinsics)
+                        GeometryUtil.intrinsics_to_matrix(intrinsics),
+                        debug=True
                     )
 
                 # TODO: Comment here.
@@ -105,13 +106,19 @@ def main() -> None:
                     # TODO
                     estimated_depth_image = np.where(estimated_depth_image <= 3.0, estimated_depth_image, 0.0)
 
-                    # Use the depth image and pose to make an Octomap point cloud.
-                    pcd: Pointcloud = OctomapUtil.make_point_cloud(estimated_depth_image, tracker_w_t_c, intrinsics)
-
-                    # Fuse the point cloud into the octree.
+                    # Fuse the frame into the TSDF.
                     start = timer()
-                    origin: Vector3 = Vector3(0.0, 0.0, 0.0)
-                    tree.insert_point_cloud(pcd, origin, discretize=True)
+
+                    height, width = estimated_depth_image.shape
+                    fx, fy, cx, cy = intrinsics
+                    o3d_intrinsics: o3d.camera.PinholeCameraIntrinsic = o3d.camera.PinholeCameraIntrinsic(
+                        width, height, fx, fy, cx, cy
+                    )
+                    ReconstructionUtil.integrate_frame(
+                        ImageUtil.flip_channels(colour_image), estimated_depth_image, np.linalg.inv(tracker_w_t_c),
+                        o3d_intrinsics, tsdf
+                    )
+
                     end = timer()
                     print(f"  - Time: {end - start}s")
 
@@ -127,12 +134,6 @@ def main() -> None:
                 # Set the projection matrix.
                 glMatrixMode(GL_PROJECTION)
                 OpenGLUtil.set_projection_matrix(intrinsics, *window_size)
-
-                # Draw the octree.
-                viewing_pose: np.ndarray = \
-                    np.linalg.inv(tracker_w_t_c) if args["camera_mode"] == "follow" \
-                    else camera_controller.get_pose()
-                OctomapUtil.draw_octree(tree, viewing_pose, drawer)
 
             # Swap the front and back buffers.
             pygame.display.flip()
