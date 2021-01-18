2df7f2b490768095f7cd5230d37de94f28cd9abb
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-18 17:07:17 +0000

    Minor tweak.

--- smg/mapping/metric/drone_fsm.py
+++ smg/mapping/metric/drone_fsm.py
@@ -98,7 +98,7 @@ class DroneFSM:
         """
         Run an iteration of the state machine.
 
-        :param image:               The most recent colour image from the drone.
+        :param image:               The most recent image from the drone.
         :param intrinsics:          The intrinsics of the drone's camera.
         :param tracker_c_t_i:       A non-metric transformation from initial camera space to current camera space,
                                     as estimated by the tracker.
