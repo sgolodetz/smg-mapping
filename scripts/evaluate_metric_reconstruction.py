77e1167c68575b3d363ce5aa93f677ed1e5cd3be
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-26 10:39:05 +0000

    Move load_fiducials to FiducialUtil.

--- scripts/evaluate_metric_reconstruction.py
+++ scripts/evaluate_metric_reconstruction.py
@@ -1,38 +1,11 @@
 import numpy as np
 import open3d as o3d
-import re
 
 from argparse import ArgumentParser
 from typing import Dict, List, Optional
 
 from smg.open3d import VisualisationUtil
-from smg.utility import GeometryUtil
-
-
-def load_fiducials(filename: str) -> Dict[str, np.ndarray]:
-    """
-    Load named fiducials from a file.
-
-    :param filename:    The name of the file containing the fiducials.
-    :return:            A dictionary mapping fiducial names to positions in 3D space.
-    """
-    fiducials: Dict[str, np.ndarray] = {}
-
-    vec_spec = r"\(\s*(.*?)\s+(.*?)\s+(.*?)\s*\)"
-    line_spec = r".*?\s+(.*?)\s+" + vec_spec + r"\s+?" + vec_spec + r"\s+?" + vec_spec + r"\s+?" + vec_spec + ".*"
-    prog = re.compile(line_spec)
-
-    def to_vec(x: int, y: int, z: int) -> np.ndarray:
-        return np.array([float(m.group(x)), float(m.group(y)), float(m.group(z))])
-
-    with open(filename, "r") as f:
-        for line in f:
-            m = prog.match(line)
-            name = m.group(1)
-            pos = to_vec(2, 3, 4)
-            fiducials[name] = pos
-
-    return fiducials
+from smg.utility import FiducialUtil, GeometryUtil
 
 
 def main() -> None:
@@ -76,7 +49,7 @@ def main() -> None:
     input_mesh: o3d.geometry.TriangleMesh = o3d.io.read_triangle_mesh(args["input_filename"])
 
     # Load in the positions of the four marker corners as estimated during the ground-truth reconstruction.
-    fiducials: Dict[str, np.ndarray] = load_fiducials(args["fiducials_filename"])
+    fiducials: Dict[str, np.ndarray] = FiducialUtil.load_fiducials(args["fiducials_filename"])
 
     # Stack these positions into a 3x4 matrix.
     p: np.ndarray = np.column_stack([
