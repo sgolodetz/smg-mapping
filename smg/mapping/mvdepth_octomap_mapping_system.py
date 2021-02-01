ad3b48a8da2da5321fbf9eff3537d4e1ab16dcf3
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-01 17:01:31 +0000

    Implement 3D object detection for the MVDepth -> Octomap mapping system.

--- smg/mapping/mvdepth_octomap_mapping_system.py
+++ smg/mapping/mvdepth_octomap_mapping_system.py
@@ -1,3 +1,5 @@
+import cv2
+import detectron2
 import numpy as np
 import os
 import time
@@ -7,10 +9,12 @@ import pygame
 
 import threading
 
+from detectron2.structures import Instances
 from OpenGL.GL import *
 from timeit import default_timer as timer
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
+from smg.detectron2 import InstanceSegmenter, ObjectDetector3D
 from smg.mapping.remote import MappingServer, RGBDFrameReceiver
 from smg.mvdepthnet import MonocularDepthEstimator
 from smg.opengl import OpenGLMatrixContext, OpenGLUtil
@@ -37,15 +41,34 @@ class MVDepthOctomapMappingSystem:
         """
         self.__camera_mode: str = "free"
         self.__depth_estimator: MonocularDepthEstimator = depth_estimator
-        self.__image_size: Optional[Tuple[int, int]] = None
-        self.__intrinsics: Optional[Tuple[float, float, float, float]] = None
         self.__output_dir: Optional[str] = output_dir
         self.__server: MappingServer = server
         self.__should_terminate: bool = False
-        self.__tracker_w_t_c: Optional[np.ndarray] = None
-        self.__tree: Optional[OcTree] = None
 
-        self.__mapping_lock: threading.Lock = threading.Lock()
+        # The image size and camera intrinsics, together with their lock.
+        self.__image_size: Optional[Tuple[int, int]] = None
+        self.__intrinsics: Optional[Tuple[float, float, float, float]] = None
+        self.__parameters_lock: threading.Lock = threading.Lock()
+
+        # The detection inputs/outputs, together with their lock.
+        self.__detection_colour_image: Optional[np.ndarray] = None
+        self.__detection_depth_image: Optional[np.ndarray] = None
+        self.__detection_tracker_w_t_c: Optional[np.ndarray] = None
+        self.__detection_lock: threading.Lock = threading.Lock()
+
+        # The mapping pose, the detected 3D objects, the most recent instance segmentation and the octree,
+        # together with their lock.
+        self.__instance_segmentation: Optional[np.ndarray] = None
+        self.__mapping_w_t_c: Optional[np.ndarray] = None
+        self.__objects: List[ObjectDetector3D.Object3D] = []
+        self.__octree: Optional[OcTree] = None
+        self.__scene_lock: threading.Lock = threading.Lock()
+
+        # The threads and conditions.
+        self.__detection_thread: Optional[threading.Thread] = None
+        self.__detection_input_ready: threading.Condition = threading.Condition(self.__detection_lock)
+        self.__detection_input_is_ready: bool = False
+
         self.__mapping_thread: Optional[threading.Thread] = None
 
     # SPECIAL METHODS
@@ -85,6 +108,10 @@ class MVDepthOctomapMappingSystem:
         self.__mapping_thread = threading.Thread(target=self.__run_mapping)
         self.__mapping_thread.start()
 
+        # Start the detection thread.
+        self.__detection_thread = threading.Thread(target=self.__run_detection)
+        self.__detection_thread.start()
+
         # Until the mapping system should terminate:
         while not self.__should_terminate:
             # Process any PyGame events.
@@ -96,6 +123,21 @@ class MVDepthOctomapMappingSystem:
                     # noinspection PyProtectedMember
                     os._exit(0)
 
+            with self.__parameters_lock:
+                # Try to get the camera parameters.
+                image_size: Optional[Tuple[int, int]] = self.__image_size
+                intrinsics: Optional[Tuple[float, float, float, float]] = self.__intrinsics
+
+            with self.__scene_lock:
+                # Try to get the mapping pose.
+                mapping_w_t_c: Optional[np.ndarray] = self.__mapping_w_t_c.copy() \
+                    if self.__mapping_w_t_c is not None else None
+
+                # Show the most recent instance segmentation (if any).
+                if self.__instance_segmentation is not None:
+                    cv2.imshow("Instance Segmentation", self.__instance_segmentation)
+                    cv2.waitKey(1)
+
             # Allow the user to control the camera.
             camera_controller.update(pygame.key.get_pressed(), timer() * 1000)
 
@@ -104,16 +146,10 @@ class MVDepthOctomapMappingSystem:
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
             # Once at least one frame has been received:
-            with self.__mapping_lock:
-                image_size: Optional[Tuple[int, int]] = self.__image_size
-                intrinsics: Optional[Tuple[float, float, float, float]] = self.__intrinsics
-                tracker_w_t_c: Optional[np.ndarray] = self.__tracker_w_t_c.copy() \
-                    if self.__tracker_w_t_c is not None else None
-
             if image_size is not None:
                 # Determine the viewing pose.
                 viewing_pose: np.ndarray = \
-                    np.linalg.inv(tracker_w_t_c) if self.__camera_mode == "follow" and tracker_w_t_c is not None \
+                    np.linalg.inv(mapping_w_t_c) if self.__camera_mode == "follow" and mapping_w_t_c is not None \
                     else camera_controller.get_pose()
 
                 # Set the projection matrix.
@@ -129,9 +165,9 @@ class MVDepthOctomapMappingSystem:
                         OpenGLUtil.render_voxel_grid([-2, -2, -2], [2, 0, 2], [1, 1, 1], dotted=True)
 
                         # Draw the octree.
-                        with self.__mapping_lock:
-                            if self.__tree is not None:
-                                OctomapUtil.draw_octree(self.__tree, drawer)
+                        with self.__scene_lock:
+                            if self.__octree is not None:
+                                OctomapUtil.draw_octree(self.__octree, drawer)
 
             # Swap the front and back buffers.
             pygame.display.flip()
@@ -142,21 +178,80 @@ class MVDepthOctomapMappingSystem:
             # TODO: Comment here.
             self.__should_terminate = True
 
-            # TODO: Comment here.
+            # Join any running threads.
+            if self.__detection_thread is not None:
+                self.__detection_thread.join()
             if self.__mapping_thread is not None:
                 self.__mapping_thread.join()
 
     # PRIVATE METHODS
+
+    def __run_detection(self) -> None:
+        """Run the detection thread."""
+        # Construct the instance segmenter and object detector.
+        instance_segmenter: InstanceSegmenter = InstanceSegmenter.make_mask_rcnn(device="cpu")
+        object_detector: ObjectDetector3D = ObjectDetector3D(instance_segmenter)
+
+        # Until termination is requested:
+        while not self.__should_terminate:
+            with self.__detection_lock:
+                # Wait for a detection request. If termination is requested whilst waiting, exit.
+                while not self.__detection_input_is_ready:
+                    self.__detection_input_ready.wait(0.1)
+                    if self.__should_terminate:
+                        return
+
+                start = timer()
+
+                # Find any 2D instances in the detection input image.
+                raw_instances: detectron2.structures.Instances = instance_segmenter.segment_raw(
+                    self.__detection_colour_image
+                )
+
+                end = timer()
+                print(f"  - Segmentation Time: {end - start}s")
+
+                # Draw the 2D instance segmentation so that it can be shown to the user.
+                instance_segmentation: np.ndarray = instance_segmenter.draw_raw_instances(
+                    raw_instances, self.__detection_colour_image
+                )
+
+                # Get the camera intrinsics.
+                with self.__parameters_lock:
+                    intrinsics: Tuple[float, float, float, float] = self.__intrinsics
+
+                start = timer()
+
+                # Lift the 2D instances to 3D objects.
+                # TODO: Ultimately, they should be fused in - this is a first pass.
+                instances: List[InstanceSegmenter.Instance] = instance_segmenter.parse_raw_instances(raw_instances)
+                instances = [instance for instance in instances if instance.label != "book"]
+                objects: List[ObjectDetector3D.Object3D] = object_detector.lift_to_3d(
+                    instances, self.__detection_depth_image, self.__detection_tracker_w_t_c, intrinsics
+                )
+
+                end = timer()
+                print(f"  - Lifting Time: {end - start}s")
+
+                with self.__scene_lock:
+                    # Add the detected 3D objects to the overall list.
+                    self.__objects += objects
+
+                    # Share the instance segmentation with other threads.
+                    self.__instance_segmentation = instance_segmentation.copy()
+
+                # Signal that the detector is now idle.
+                self.__detection_input_is_ready = False
 
     def __run_mapping(self) -> None:
         """Run the mapping thread."""
         client_id: int = 0
         receiver: RGBDFrameReceiver = RGBDFrameReceiver()
 
-        # Create the octree.
+        # Construct the octree.
         voxel_size: float = 0.05
-        self.__tree = OcTree(voxel_size)
-        self.__tree.set_occupancy_thres(0.8)
+        self.__octree = OcTree(voxel_size)
+        self.__octree.set_occupancy_thres(0.8)
 
         # Until termination is requested:
         while not self.__should_terminate:
@@ -166,8 +261,8 @@ class MVDepthOctomapMappingSystem:
                 height, width, _ = self.__server.get_image_shapes(client_id)[0]
                 intrinsics: Tuple[float, float, float, float] = self.__server.get_intrinsics(client_id)[0]
 
-                # Record them so that the main thread has access to them.
-                with self.__mapping_lock:
+                # Record them so that other threads have access to them.
+                with self.__parameters_lock:
                     self.__image_size = (width, height)
                     self.__intrinsics = intrinsics
 
@@ -179,9 +274,9 @@ class MVDepthOctomapMappingSystem:
                 colour_image: np.ndarray = receiver.get_rgb_image()
                 tracker_w_t_c: np.ndarray = receiver.get_pose()
 
-                # Record the pose so that the main thread has access to it.
-                with self.__mapping_lock:
-                    self.__tracker_w_t_c = tracker_w_t_c.copy()
+                # Record the mapping pose so that other threads have access to it.
+                with self.__scene_lock:
+                    self.__mapping_w_t_c = tracker_w_t_c.copy()
 
                 # Try to estimate a depth image for the frame.
                 estimated_depth_image: Optional[np.ndarray] = self.__depth_estimator.estimate_depth(
@@ -199,12 +294,25 @@ class MVDepthOctomapMappingSystem:
                     # Fuse the point cloud into the octree.
                     start = timer()
 
-                    with self.__mapping_lock:
+                    with self.__scene_lock:
                         origin: Vector3 = Vector3(0.0, 0.0, 0.0)
-                        self.__tree.insert_point_cloud(pcd, origin, discretize=True)
+                        self.__octree.insert_point_cloud(pcd, origin, discretize=True)
 
                     end = timer()
-                    print(f"  - Time: {end - start}s")
+                    print(f"  - Fusion Time: {end - start}s")
+
+                    # If no frame is currently being processed by the 3D object detector, schedule this one.
+                    acquired: bool = self.__detection_lock.acquire(blocking=False)
+                    if acquired:
+                        try:
+                            if not self.__detection_input_is_ready:
+                                self.__detection_colour_image = colour_image.copy()
+                                self.__detection_depth_image = estimated_depth_image.copy()
+                                self.__detection_tracker_w_t_c = tracker_w_t_c.copy()
+                                self.__detection_input_is_ready = True
+                                self.__detection_input_ready.notify()
+                        finally:
+                            self.__detection_lock.release()
             else:
                 # If no new frame is currently available, wait for 10ms to avoid a spin loop.
                 time.sleep(0.01)
